f5b093b020b3d24a4c692e30cc838fc8ffea7a79
Juliette Engelaere-Lefebvre <juliette.engelaere@developpement-durable.gouv.fr>
2021-12-09 17:08:39 +0100

    Intégration savoirfR Passage du repertoire data/ en en extdata/ pour conformité nommage savoirfR Bascule de l'appel des library au niveau de get-started (comme ds M5)

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -6,26 +6,27 @@ library(stringr)
 library(readr)
 library(readxl)
 library(tidyverse)
-epci_geo<-st_read(dsn="extdata/epci/",layer="EPCI")
 
-departements_geo<-st_read(dsn="extdata/departements/",layer="DEPARTEMENT") %>% 
-  mutate(AREA=st_area(geometry))
+epci_geo <- st_read(dsn = "extdataraw/epci/", layer = "EPCI")
 
-regions_geo<-st_read(dsn="extdata/regions/",layer="REGION")
+departements_geo <- st_read(dsn = "extdataraw/departements/", layer = "DEPARTEMENT") %>%
+  mutate(AREA = st_area(geometry))
 
-prefecture_de_region_geo<-st_read(dsn="extdata/adminexpress/",layer="CHEF_LIEU") %>% 
-  filter(STATUT == "Préfecture de région") %>% 
-  mutate(coords=as.character(geometry) %>% 
-           str_replace_all("c\\(","") %>% 
-           str_replace_all("\\)","")) %>% 
-  mutate(x=str_split_fixed(coords,", ",2)[,1] %>% 
-           str_trim(),
-         y=str_split_fixed(coords,", ",2)[,2] %>% 
-           str_trim()
-         ) %>% 
-  select(-coords) %>% 
+regions_geo <- st_read(dsn = "extdataraw/regions/", layer = "REGION")
+
+prefecture_de_region_geo <- st_read(dsn = "extdataraw/adminexpress/", layer = "CHEF_LIEU") %>%
+  filter(STATUT == "Préfecture de région") %>%
+  mutate(coords = as.character(geometry) %>%
+    str_replace_all("c\\(", "") %>%
+    str_replace_all("\\)", "")) %>%
+  mutate(x = str_split_fixed(coords, ", ", 2)[, 1] %>%
+    str_trim(),
+  y = str_split_fixed(coords, ", ", 2)[, 2] %>%
+    str_trim()
+  ) %>%
+  select(-coords) %>%
   st_drop_geometry()
 
-write_csv2(prefecture_de_region_geo,"extdata/prefecture.csv")
+write_csv2(prefecture_de_region_geo, "extdataraw/prefecture.csv")
 
-save(epci_geo,departements_geo,regions_geo,file="data/admin_express.RData")
+save(epci_geo, departements_geo, regions_geo, file = "data/admin_express.RData")
