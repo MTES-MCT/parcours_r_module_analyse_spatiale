f954cfa29c0e52ca4110ea774fcd94a058b9d57d
Juliette Engelaere-Lefebvre <37416795+jengelaere@users.noreply.github.com>
2023-01-07 15:28:15 +0100

    Dev (#51)
    * Supression renv, actualisation DESCRIPTION adaptation CI, renommage des pages html pour faciliter la création du book pdf, add new CI - R 4.2.1  
    * Rapatriement de tous les packages au début, mise dans l'ordre l'alphabétique, suppression des doublons avec library(tidyverse) - Tri des packages utilisés en début de chap selon leur source (CRAN ou autre)  
    
    * Intégration savoirfR    
      - Passage du repertoire data/ en en extdata/ pour conformité nommage savoirfR  
      - mise en forme exercices.Rmd et ajout de la positions des 5 exercices dans le support de cours  
      - ajout savoirfR comme dépendances  
      - ajout exercice 1 à 5 de savoirfR
      - ajout chap exercices corrigés
    
    * ajout des parties communes de parcours-r
    
    * maj image onyxia de base et version R
    
    Révision et enrichissement du contenu ; 
    * ajout COGiter et Cartelette chap 4, 
    * Ajout exemple st_write au format csv §4.2
    * ajout écriture tidyverse pour filtrer avec un prédicat géométrique 
    * remplace F par FALSE 
    * ajout méthode avec st_filter 
    * présentation datalibaba 
    * chap 8 différences long/lat - CRS projeté : ajout exemple de mesure de surf pour insister sur pourquoi reprojeter, Présentation 'of_largest_polygon' (closes #16).  
    * ajout geocodage et numerisation (chap 9), 
    * remembrement ggplot2 : présentation {gouvdown}, {ggiraph} et {mapfactory}, ajout exemple scale_gradient avec gouv_colors()
    * ajout focus sur OSM après les cartes web. 
    * sf 0.9 : mise à jour stat_sf_coordinates, plus besoin de passer par les centroîdes, Adaptation du passage sur la différence entre coordonnées géo et coord projetée (§8.1) 
    * Reprise exos (#48)   Suppression patchwork, remplacé par cowplot présenté dans le support.
    * Passage de tmap en annexe, renumérotation des chapitres, reprise cartogrammes et carto web
    
    * nommage du premier chunk de chaque chapitre pour s'y repérer quand ça compile, 
    
    * Menage dans les images inutilisées
    
    * suppression de la mention à {maptiles} qui n'était pas utilisé
    
    * :bug: fix CI  : les matrices de calculs osrm n'aboutissent pas, j'ai triché avec un RData pour restituer le résultat
    
    * ajout des exemples pour lires des données du web
    
    * rajoute les hyperliens sur la partie lires des données web
    
    * autorisation erreur banR

--- data-raw/support_pdf.R
+++ data-raw/support_pdf.R
@@ -10,7 +10,11 @@ chap <- c("index.Rmd", list.files(".", ".Rmd")) %>%
   gsub("^[[:digit:]]*-", "", .)
 
 
-propre.rpls::creer_pdf_book(nom_pdf = "Parcours_R_support_m7_Analyses_spaciales.pdf",
+propre.rpls::creer_pdf_book(nom_pdf = "Parcours_R_support_m7_Analyses_spatiales.pdf",
                             pages_html = chap)
 
-"1-25;28-39;51-60;72-136;138-146"
+# Des pages blanches ajoutées inutilement
+# Pour imprimer, garder les pages
+# "1-30;33-48;60-69;81-91;99-153"
+# supprimer : 
+# 31-32;49-59;70-80;92-98
